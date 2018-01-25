cd3bb9443ab602a57cca0d0fe38db03f657a3185
stoversa <stoversa@gmail.com>
2018-01-25 00:07:07 -0500

    Added Firebase. Still in progress

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
+    const config = {
+        apiKey: "AIzaSyCzc_uB6Vm0fLjvg8-I49W197kVM5Y2keQ",
+        authDomain: "rps-multiplayer-33e8c.firebaseapp.com",
+        databaseURL: "https://rps-multiplayer-33e8c.firebaseio.com",
+        projectId: "rps-multiplayer-33e8c",
+        storageBucket: "rps-multiplayer-33e8c.appspot.com",
+        messagingSenderId: "187149775253"
+    };
+    firebase.initializeApp(config);
+    const database = firebase.database();
+    const usersOne = database.ref("user1");
+    const userTwo = database.ref("user2");
+    const chatDB = database.ref("chat");
+
     var game = {
         playerOneName: "",
         playerTwoName: "",
@@ -10,16 +24,35 @@ $(document).ready(function(){
         winsPlayerTwo: 0,
         lossesPlayerTwo: 0,
         ties: 0,
+        displayModal: function () {
+            if (game.playerOneName === "" && game.playerTwoName === "") { //if the first player has not given a name and effectively logged in, display error warning
+                $('#myModal').modal('show');
+                return
+            }
+        },
+        addChat: function () {
+            if (game.playerOneName === "") { //if the first player has not given a name and effectively logged in, display error warning
+                $('#myModal').modal('show');
+            }
+            else {
+                var chatText = $("#chat-input").val().trim(); //grabs chat input
+                $("#chat-input").val("");
+                chatDB.push(game.playerOneName + ": " + chatText);
+                $(".chat-area").empty();
+                game.populateChat();
+            }
+        }, 
         populate: function (){
             if (game.playerOneName !== ""){
                 $(".player-one-title").text(game.playerOneName);
                 $(".player-one-img").removeClass("hidden");
-                
             }
+
             if (game.playerTwoName !== "") {
                 $(".player-two-title").text(game.playerTwoName);
                 $(".player-two-img").removeClass("hidden");
             }
+
             if (game.playerOneName != "" && game.playerTwoName != "") {
                 $(".name-row").replaceWith("<div class='game-status text-center'></div>") //hides name field
                 $(".player-one-img").addClass("hidden");
@@ -28,33 +61,37 @@ $(document).ready(function(){
                 $(".player-two-options").removeClass("hidden");
             };
         },
-        displayModal: function () {
-            if (game.playerOneName === "" && game.playerTwoName === "") { //if the first player has not given a name and effectively logged in, display error warning
-                $('#myModal').modal('show');
-                return
-            }
+        populateChat: function(){
+            chatDB.on("child_added", function (snapshot) {
+                var newLine = $("<div>").text(snapshot.val());
+                $(".chat-area").prepend(newLine);
+            }, function (err) {
+                // Handle errors
+                console.log("Error: ", err.code);
+            });
         },
+
         beginGame: function(){
             var name = $("#name-input").val().trim(); //grabs the name user inputs
+            $("#name-input").val("");
             if (game.playerOneName === ""){
                 game.playerOneName = name;
+                usersOne.set({
+                        name: game.playerOneName,
+                        wins: game.winsPlayerOne,
+                        losses: game.lossesPlayerOne
+                });
             }
             else {
                 game.playerTwoName = name;
+                userTwo.set({
+                        name: game.playerTwoName,
+                        wins: game.winsPlayerTwo,
+                        losses: game.lossesPlayerTwo
+                });
             }
             game.populate();
         },
-        addChat: function(){
-            if (game.playerOneName === "") { //if the first player has not given a name and effectively logged in, display error warning
-                $('#myModal').modal('show');
-            }
-            else {
-            var chatText = $("#chat-input").val().trim(); //grabs chat input
-            $("#chat-input").val("");
-            var newLine = $("<div>").text(game.playerOneName + ": " + chatText);
-            $(".chat-area").append(newLine);
-            }
-        }, 
         logChoice: function(){
             var userChoice = $(this).attr("data");
             var player = $(this).attr("player");
@@ -73,43 +110,54 @@ $(document).ready(function(){
             }
         },
         gradeGame: function(){
+            $("#placeholder-img").hide();
+            var results = $("<h2 class='text-center' id='win-text'></h2>");
+            $(".game-results").append(results);
             if ((game.choicePlayerOne === "rock") || (game.choicePlayerOne === "paper") || (game.choicePlayerOne === "scissors")) {
                 if ((game.choicePlayerOne === "rock") && (game.choicePlayerTwo === "scissors")) {
                     game.winsPlayerOne++;
                     game.lossesPlayerTwo++;
-                    console.log(game.playerOneName +  " Wins!");
+                    results.text(game.playerOneName +  " Wins!");
                 } else if ((game.choicePlayerOne === "rock") && (game.choicePlayerTwo === "paper")) {
                     game.lossesPlayerOne++;
                     game.winsPlayerTwo++;
-                    console.log(game.playerTwoName + " Wins!");
+                    results.text(game.playerTwoName + " Wins!");
                 } else if ((game.choicePlayerOne === "scissors") && (game.choicePlayerTwo === "rock")) {
                     game.lossesPlayerOne++;
                     game.winsPlayerTwo++;
-                    console.log(game.playerTwoName + " Wins!");
+                    results.text(game.playerTwoName + " Wins!");
                 } else if ((game.choicePlayerOne === "scissors") && (game.choicePlayerTwo === "paper")) {
                     game.winsPlayerOne++;
                     game.lossesPlayerOne++;
-                    console.log(game.playerOneName + " Wins!");
+                    results.text(game.playerOneName + " Wins!");
                 } else if ((game.choicePlayerOne === "paper") && (game.choicePlayerTwo === "rock")) {
                     game.winsPlayerOne++;
                     game.lossesPlayerOne++;
-                    console.log(game.playerOneName + " Wins!");
+                    results.text(game.playerOneName + " Wins!");
                 } else if ((game.choicePlayerOne === "paper") && (game.choicePlayerTwo === "scissors")) {
                     game.lossesPlayerOne++;
                     game.winsPlayerTwo++;
-                    console.log(game.playerTwoName + " Wins!");
+                    results.text(game.playerTwoName + " Wins!");
                 } else if (game.choicePlayerOne === game.choicePlayerTwo) {
-                    game.ties++;
+                    results.text("It's a tie!");
                 };
-                
-                $(".player-one-options").removeClass("hidden");
-                $(".player-two-options").removeClass("hidden");
+                setTimeout(game.reset, 3000);
             }
+        },
+        reset: function(){
+            $(".choice").text("");
+            game.choicePlayerOne = "";
+            game.choicePlayerTwo = "";
+            $("#placeholder-img").show();
+            $("#win-text").remove();
+            $(".player-one-options").removeClass("hidden");
+            $(".player-two-options").removeClass("hidden");
         }
     };
 
 
     game.populate();
+    game.populateChat();
     $(".btn-name").click(game.beginGame);
     $(".btn-chat").click(game.addChat);
     $(".btn-rps").click(game.logChoice);
